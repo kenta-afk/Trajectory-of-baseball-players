5093eb8fbd40f0574c38528e3aacae3d9b6330d2
kenta-afk <kentarou9.60@icloud.com>
2024-04-01 13:42:37 +0900

    first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,10 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
+
+    def after_sign_in_path_for(resource)
+      top_page_index_path # ログイン後にリダイレクトしたいpathを指定
+    end
+    
   end
   
