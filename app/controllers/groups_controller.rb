5739b5cc2926ed915fef7038e72777c459a089d5
kenta-afk <kentarou9.60@icloud.com>
2024-04-13 12:21:33 +0900

    modify only group_user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,34 +1,34 @@
 class GroupsController < ApplicationController
     def index
-        @groups = Group.all
+      @groups = current_user.groups
     end
-
+  
     def new
-        @group = Group.new
-        @group.build_status
+      @group = Group.new
+      @group.build_status
     end
-
+  
     def create
-        
-        @group = Group.new(group_params)
-    
-        if @group.save
-          redirect_to @group, notice: 'Group was successfully created.'
-        else
-          render :new
-        end
+      @group = Group.new(group_params)
+  
+      if @group.save
+        # グループを作成したユーザーをメンバーとして追加
+        @group.group_users.create(user: current_user)
+        redirect_to @group, notice: 'Group was successfully created.'
+      else
+        render :new
+      end
     end
-    
+  
     def show
-        @group = Group.find(params[:id])
-        @members = @group.group_users
+      @group = Group.find(params[:id])
+      @members = @group.group_users
     end
-      
-
+  
     private
-
+  
     def group_params
-        params.require(:group).permit(:name)
-        params.require(:group).permit(:name, status_attributes: [:user_id, :group_id])
+      params.require(:group).permit(:name, status_attributes: [:user_id, :group_id])
     end
-end
+  end
+  
