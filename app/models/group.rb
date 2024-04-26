fba1f9a1141e7202d117109093a224dfeede04e7
kenta-afk <kentarou9.60@icloud.com>
2024-04-26 09:30:52 +0900

    modify relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,17 +1,14 @@
-# app/models/group.rb
 class Group < ApplicationRecord
-    before_create :generate_uuid
-  
-    has_many :group_users, dependent: :destroy
-    has_many :users, through: :group_users
-    has_many :statuses
-    has_many :users, through: :statuses
-    accepts_nested_attributes_for :statuses
-  
-    private
-  
-    def generate_uuid
-      self.uuid = SecureRandom.uuid
-    end
+  before_create :generate_uuid
+
+  has_many :group_users, dependent: :destroy
+  has_many :users, through: :group_users
+  has_many :statuses
+  accepts_nested_attributes_for :statuses
+
+  private
+
+  def generate_uuid
+    self.uuid = SecureRandom.uuid
+  end
 end
-  
